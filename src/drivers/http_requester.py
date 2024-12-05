274513224fa4f7e48bf46b01a40d3568fd1b62e6
Karine Yasmin <karine.yasmin@outlook.com>
2024-12-05 20:37:20 -0300

    config: Removing pre-comit

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -7,5 +7,5 @@ class HttpRequester:
         self.__url = "https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm"
 
     def request_from_page(self):
-        response = requests.get(self.__url)
+        response = requests.get(self.__url, timeout=10)
         return {"status_code": response.status_code, "html": response.text}
