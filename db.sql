3963d48bfd1c3c3c6ff52e83a32876ff20bf0b07
Karine Yasmin <karine.yasmin@outlook.com>
2024-12-07 20:24:54 -0300

    fix: property name

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `pipeline_db`.`artists` (
     id BIGINT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255),
     second_name VARCHAR(255),
-    surname VARCHAR(255),
+    nickname VARCHAR(255),
     artist_id BIGINT,
     link VARCHAR(255),
     extraction_date DATETIME NOT NULL,
